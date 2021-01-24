a803a59b70b01bba4dafbafc0be81f2ed8a6bb84
Filipe de Oliveira Barbosa <filipe_oli@hotmaiil.com>
2021-01-24 04:17:08 -0300

    Teste

--- CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
+++ CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -6,12 +6,12 @@ namespace CursoCSharp.ClassesEMetodos
 {
     class DesafioAtributo
     {
-        int a = 10;
+        int mePegue = 10;
         public static void Executar()
         {
             // Acessar a variável 'a' dentro do método Executar!
             DesafioAtributo desafioEasy = new DesafioAtributo();    // É necessário instanciar pois estou tentando acessar um item não static, dentro de uma função static.
-            Console.WriteLine(desafioEasy.a);
+            Console.WriteLine(desafioEasy.mePegue);
         }
     }
 }
